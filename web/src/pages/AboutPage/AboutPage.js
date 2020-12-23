4538e7617a8d4795ca439580c85aa180cbcd2499
choilm <choilmto@gmail.com>
2020-12-23 13:37:28 -0500

    Add layout

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,17 +1,13 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const AboutPage = () => {
   return (
-    <>
-      <h1>AboutPage</h1>
+    <BlogLayout>
       <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.js</code>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
       </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
-      </p>
-    </>
+    </BlogLayout>
   )
 }
 
